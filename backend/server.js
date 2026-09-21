4dce38555bb03f2e7d495cbded5ee2e02c11ec1a
riahi aziz <riahiaziz199@gmail.com>
2026-09-21 05:20:48 +0100

    scurety 'anti bots ..' and start on build the frontend

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,42 @@ const app=express();
 const PORT = process.env.PORT || 3000;
 
 console.log(PORT);
-
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
 app.use(morgan("dev"));
+
+app.use(async(req,res,next) => {
+    try {
+        const decision = await aj.protect(req, {
+            requested:1 // specifies that each request consumes 1 token
+        })
+        
+        if (decision.isDenied()){
+            if(decision.reason.isRateLimit()){
+                res.status(429).json({error:"Too many Requests"});
+            }else if (decision.reason.isBot()){
+                res.status(403).json({error:"Bot access denied"});
+            }else {
+                res.status(403).json({error:"Forbidden"});
+            }
+        return;
+        }
+
+        // check for spoofed bots
+
+        if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())){
+            res.status(403).json({error: "spoofed bot detected"});
+            return;
+        }
+
+        next()
+    } catch (error) {
+        console.log("Arcjet error",error);
+        next(error);
+    }
+});
 
 app.use("/api/products", productRouts);
 
